fc31f1a3cca10b410b03806ca34a117b5b9a1d94
Eduardo Brito <up201806271@fe.up.pt>
2020-10-21 12:11:42 +0100

    Changed Border to Wall & Other Symbols

--- src/mapello.pl
+++ src/mapello.pl
@@ -1,24 +1,23 @@
 % Atoms
-code(wall,'/').
-code(empty,'.').
+code(empty,' ').
 code(bonus, '*').
 code(joker,'J').
-code(border,'#').
-code(white, 'o').
+code(wall,'#').
+code(white, 'W').
 code(black, 'B').
 
 %Board -  Hard Coded Board
 initial([
-[border, border, border, joker, border, border, joker, border, border, border],
-[border, wall, empty, bonus, empty, bonus, empty, empty, wall, border],
-[border, empty, empty, empty, empty, empty, wall, empty, bonus, joker],
-[border, bonus, wall, empty, empty, empty, empty, empty, empty, border],
-[border, empty, empty, empty, black, white, empty, empty, empty, border],
-[joker, empty, empty, empty, white, black, empty, empty, empty, border],
-[border, bonus, empty, empty, empty, empty, empty, wall, bonus, joker],
-[border, empty, empty, wall, empty, empty, empty, empty, empty, border],
-[border, wall, empty, bonus, empty, empty, bonus, empty, wall, border],
-[border, border, border, joker, joker, border, border, joker, border, border]
+[wall, wall, wall, joker, wall, wall, joker, wall, wall, wall],
+[wall, wall, empty, bonus, empty, bonus, empty, empty, wall, wall],
+[wall, empty, empty, empty, empty, empty, wall, empty, bonus, joker],
+[wall, bonus, wall, empty, empty, empty, empty, empty, empty, wall],
+[wall, empty, empty, empty, black, white, empty, empty, empty, wall],
+[joker, empty, empty, empty, white, black, empty, empty, empty, wall],
+[wall, bonus, empty, empty, empty, empty, empty, wall, bonus, joker],
+[wall, empty, empty, wall, empty, empty, empty, empty, empty, wall],
+[wall, wall, empty, bonus, empty, empty, bonus, empty, wall, wall],
+[wall, wall, wall, joker, joker, wall, wall, joker, wall, wall]
 ]).
 
 % Draw Functions
